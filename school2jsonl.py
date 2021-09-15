88a4d5369d4a4e72c9d3bcb3d472665f217f3efe
Mwang413 <71150326+Mwang413@users.noreply.github.com>
2021-09-15 18:34:07 -0500

    done

--- school2jsonl.py
+++ school2jsonl.py
@@ -1,16 +1,24 @@
 import json
 import jsonlines
 
-path = input("path: ")
 
-def description2jsonl(path):
-     with open(path) as f:
-          f = json.load(f)
-          with jsonlines.open("train.jsonl", "w") as w:
-               for i in range(int(list(f.keys())[-1][7])): #Get last item for number of schools
-                    f[f"school {str(i+1)} description"]
-                    desc = f[f"school {str(i+1)} description"]
-                    target = "ideal generated text"
-                    w.write({f"{desc}": target})
+def description2jsonl():
+     try:
+          path = input("Path of school json file: ")
+          path_to_json = input("Path to new jsonl train file: ")
+          with open(path) as f:
+               f = json.load(f)
+               with jsonlines.open(path_to_json, "w") as w:
+                    for i in range(int(list(f.keys())[-1][7])): #Get last item for number of schools
+                         f[f"school {str(i+1)} description"]
+                         desc = f[f"school {str(i+1)} description"]
+                         w.write({"prompt": f"{desc}", "completion": "<ideal generated text>"})
+     except:
+          print("Retry.")
+     
 
-description2jsonl(path)
+description2jsonl()
+
+with open("/Users/muduo/Documents/GitHub/syllabus-extraction/train.jsonl") as f:
+     for line in f:
+          print(line)
