07fa78fe527efda528e98ed70ed77e9e5dfb109f
Mwang413 <71150326+Mwang413@users.noreply.github.com>
2021-09-15 17:51:20 -0500

    push

--- school2jsonl.py
+++ school2jsonl.py
@@ -1,16 +1,16 @@
 import json
 import jsonlines
 
-path = input("path")
+path = input("path: ")
 
-with open(path) as f:
-     f = json.load(f)
-     with jsonlines.open("train.jsonl", "w") as w:
-          # iterate over each json-object
-          for item in f["_default"].items():
-               i=1
-               desc = item[f"school {str(0+i)} description"]
-               target = "ideal generated text"
-               w.write({f"{desc}": target})
-               i+=1
+def description2jsonl(path):
+     with open(path) as f:
+          f = json.load(f)
+          with jsonlines.open("train.jsonl", "w") as w:
+               for i in range(int(list(f.keys())[-1][7])): #Get last item for number of schools
+                    f[f"school {str(i+1)} description"]
+                    desc = f[f"school {str(i+1)} description"]
+                    target = "ideal generated text"
+                    w.write({f"{desc}": target})
 
+description2jsonl(path)
