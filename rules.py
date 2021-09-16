90a158393d7132fe5722a5a63083a2ccf234fea6
Matt-Payne <matthew.payne.15@cnu.edu>
2021-09-16 12:52:07 -0400

    matt rules init

--- rules.py
+++ rules.py
@@ -11,7 +11,7 @@ os.chdir('/Users/muduo/Documents/syllabus-extraction')
 
 class match_with_rules(object):
      wlist = ("Grade", "graduate", "undergraduate", "course", "class", 'late', 'late work', "syllabus", "withdraw policy", "course withdraw", "withdrawing from course", "weekly", "assignments", "week", "day", "content", "description", "materials", "outcomes", "assignment", "grade", "university")
-     
+
      noun_adp_noun = [
           {"POS": {"IN": ["NOUN", "PROPN"]}, "LOWER": {"NOT_IN": wlist}, "LEMMA": {"NOT_IN": wlist}},
           {"POS": {"IN": ["ADP"]}},
@@ -23,7 +23,7 @@ class match_with_rules(object):
           {"POS": {"IN": ["NOUN", "PROPN"]}, "LOWER": {"NOT_IN": wlist}, "LEMMA": {"NOT_IN": wlist}},
           {"POS": {"IN": ["NOUN", "PROPN"]}, "LOWER": {"NOT_IN": wlist}, "LEMMA": {"NOT_IN": wlist}}
      ]
-     
+
      verb_pobj = [
           {"POS": "verb", "LOWER": {"NOT_IN": wlist}, "LEMMA": {"NOT_IN": wlist}},
           {"DEP": "pobj", "LOWER": {"NOT_IN": wlist}, "LEMMA": {'NOT_IN': wlist}}
@@ -34,7 +34,7 @@ class match_with_rules(object):
           {"DEP": "compound"},
           {"DEP": "dobj", "LOWER": {"NOT_IN": wlist}, "LEMMA": {'NOT_IN': wlist}}
           ]
-          
+
 
      m_tool = Matcher(nlp.vocab)
      m_tool.add('noun_adp_p/noun', [noun_adp_noun])
@@ -65,3 +65,37 @@ spacy.explain("dobj")
 FOCUS ON KEYWORDS
 
 `An general introduction to using POS POS in...`
+
+
+### Matt rules:
+# this is a creative writing course
+course = [
+
+          {"LOWER": "this"},
+          {"LOWER": "is"},
+          {"LOWER": {"IN": ["a", "the"]}},
+          {"POS": {"IN": ["NOUN", "PROPN", "ADJ"]}},
+          {"POS": {"IN": ["NOUN", "PROPN"]}},
+          {"LOWER": {"IN": ["course", "class"]}}
+]
+
+# this course presents topics in proportional reasoning
+presents = [
+            {"LOWER": "this"},
+            {"LOWER": {"IN": ["course", "class"]}},
+            {"LEMMA": {"IN": ["presents", "present"]}},
+            {"LOWER": "topics"},
+            {"LOWER": {"IN": ["in", "on", "about", "with", "by", "for", "at"]}},
+            {"POS": {"IN": ["NOUN", "PROPN", "ADJ"]}},
+            {"POS": {"IN": ["NOUN", "PROPN"]}}
+]
+
+# Study the concepts of injury prevention
+study = [
+         {"LOWER": {"IN": ["study", "analyze", "define", "evaluate"]}},
+         {"LEMMA": {"IN": ["the", "a", "in"]}},
+         {"LOWER": {"IN": ["concepts", "concept"]}},
+         {"LEMMA": {"IN": ["of", "on", "in", "related"]}},
+         {"POS": {"IN": ["NOUN", "PROPN", "ADJ"]}},
+         {"POS": {"IN": ["NOUN", "PROPN"]}}
+]
